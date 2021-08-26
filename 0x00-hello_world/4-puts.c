78bf812b5f095b2685f2e94c7e35a2b9e2d00664
Bello Emmanuel Oluwatimilehin <59453152+Timilehin-bello@users.noreply.github.com>
2021-08-26 20:18:33 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-nclude <stdio.h>
+#include <stdio.h>
+/**
+ * main - Prints string
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	return (0);
 }
