ccf2ca8f595db14f6bd40959af29205e30f2aa19
Timilehin-bello <emmatimmybello@gmail.com>
2021-08-31 11:29:02 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
