d973b7d85e22421d44b25bb5fabddc02df099617
Timilehin-bello <emmatimmybello@gmail.com>
2021-08-31 12:18:17 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * add - Adds two values
+  * @n1: Integer one
+  * @n2: integer two
+  *
+  * Return: Integer value
+  */
+int add(int num1, int num2)
+{
+
+	int sum = num1 + num2;
+
+	return (sum);
+}
