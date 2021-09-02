0d4830e14f7faccf87aa45507b7c80cd37034930
Timilehin-bello <emmatimmybello@gmail.com>
2021-09-02 10:47:18 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _isupper - Checks for uppercase 
+  * @c: The number to be checked
+  *
+  * Return: 1 for upper letter or 0 for any else
+  */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
+}
