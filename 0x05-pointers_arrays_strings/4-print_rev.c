90c58cc3cb6be9a538f3aeff9bb2fe9d6cf0b3ed
Timilehin-bello <emmatimmybello@gmail.com>
2021-09-06 13:32:55 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#incldue "main.h"
+#include <stdio.h>
+
+/**
+  * print_rev - Prints a string in reverse
+  * @s: The string to print
+  *
+  * Return: void
+  */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
