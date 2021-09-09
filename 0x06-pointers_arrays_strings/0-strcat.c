10ead3ecb36d374c71d7cd3ec62d8f2f292f082c
Timilehin-bello <emmatimmybello@gmail.com>
2021-09-09 12:14:58 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: destination string; src string is appended to it
+ *
+ * @src: Source string; gets appended to dest string
+ *
+ * Return: Pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int j = 0, i;
+
+	while (dest[dlen])
+	{
+		j++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[j] = src[i];
+		j++;
+	}
+
+	dest[j] = '\0';
+	return (dest);
+}
