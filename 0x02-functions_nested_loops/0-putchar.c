b2bcea9e22fcf0de8ec7b8b59864ee8d9a84a20e
Timmy <emmatimmybello@gmail.com>
2022-03-15 22:38:22 +0000

    Made changes to task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,19 @@
   */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char *s;
+	int i;
+
+	s = "_putchar";
+	i = 0;
+
+	while(i < 8)
+	{
+		_putchar(s[i]);
+		i++;
+	}
+
+
 	_putchar('\n');
 
 	return (0);
