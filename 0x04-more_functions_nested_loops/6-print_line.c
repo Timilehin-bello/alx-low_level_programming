9150896d1e179bb6edc7d871a2159d20efe74cdc
Bello Emmanuel Oluwatimilehin <59453152+Timilehin-bello@users.noreply.github.com>
2022-03-18 09:25:20 +0100

    refactor code

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,17 @@
 
 void print_line(int n)
 {
-	int i;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		int i = 0;
+		while (i < n)
 		{
 			_putchar(95);
+			i++;
 		}
 
 		_putchar('\n');
