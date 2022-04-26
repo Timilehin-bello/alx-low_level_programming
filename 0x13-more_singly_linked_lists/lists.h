74dd2c4159eef19d8d82c0e79e5765a385b3bbea
Bello Emmanuel Oluwatimilehin <59453152+Timilehin-bello@users.noreply.github.com>
2022-04-26 19:07:08 +0100

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -35,5 +35,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
-
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 #endif /* LISTS_H */
