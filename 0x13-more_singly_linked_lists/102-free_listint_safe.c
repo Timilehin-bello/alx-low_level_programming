812a5883358a3db19da1ad6816c2a652ca20dcb8
Timilehin-bello <emmatimmybello@gmail.com>
2022-04-26 23:35:31 +0100

    Made changes to task 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,28 +1,80 @@
 #include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
- * free_listint_safe - free a `listint_t` list and set the head to null
- * @h: double pointer to head of linked list
- * Description: This function should work for circular lists
- * Only loop through the list once
- * Return: size of the list that was free'd
- */
+  * free_listint_safe - frees a list including those with a loop
+  * @h: the list
+  * Return: nothing
+  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *hold;
-	size_t count;
+	listint_t *h_hold, *loop;
+	size_t len;
+	int traversed;
 
-	count = 0;
-	current = *h;
-	while (current != NULL)
+	loop = find_listint_loop(*h);
+	traversed = len = 0;
+	if (*h == NULL)
+		return (len);
+
+	while (*h)
 	{
-		count++;
-		hold = current;
-		current = current->next;
-		free(hold);
-		if (hold < current)
+		if (*h == NULL || *h == loop)
+		{
+			traversed++;
 			break;
+		}
+		h_hold = *h;
+		*h = (*h)->next;
+
+		free(h_hold);
+		len++;
+	}
+
+	while (*h)
+	{
+		if ((*h)->next == loop && traversed == 1)
+		{
+			free(*h);
+			break;
+		}
+		h_hold = *h;
+		*h = (*h)->next;
+
+		free(h_hold);
+		len++;
 	}
 	*h = NULL;
 
-	return (count);
+	return (len);
+}
+
+/**
+  * find_listint_loop - checks whether a linked list contains a loop or not
+  * @head: head of the loop
+  * Return: 1 if there is a loop or 0 if not
+  */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow_p, *fast_p;
+
+	slow_p = fast_p = head;
+	while (slow_p && fast_p && fast_p->next)
+	{
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+		if (slow_p == fast_p)
+		{
+			slow_p = head;
+			while (slow_p != fast_p)
+			{
+				slow_p = slow_p->next;
+				fast_p = fast_p->next;
+			}
+			return (fast_p);
+		}
+	}
+
+	return (NULL);
 }
