19333a4f169f62b643c4bdeef67bcd85a3673c61
Timilehin-bello <emmatimmybello@gmail.com>
2022-04-26 23:47:47 +0100

    Made changes to task 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,80 +1,64 @@
 #include "lists.h"
-#include <stddef.h>
 #include <stdlib.h>
 
 /**
-  * free_listint_safe - frees a list including those with a loop
-  * @h: the list
-  * Return: nothing
-  */
-size_t free_listint_safe(listint_t **h)
+ * _ra - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
+ */
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
-	listint_t *h_hold, *loop;
-	size_t len;
-	int traversed;
+listint_t **newlist;
+size_t i;
 
-	loop = find_listint_loop(*h);
-	traversed = len = 0;
-	if (*h == NULL)
-		return (len);
-
-	while (*h)
-	{
-		if (*h == NULL || *h == loop)
-		{
-			traversed++;
-			break;
-		}
-		h_hold = *h;
-		*h = (*h)->next;
-
-		free(h_hold);
-		len++;
-	}
-
-	while (*h)
-	{
-		if ((*h)->next == loop && traversed == 1)
-		{
-			free(*h);
-			break;
-		}
-		h_hold = *h;
-		*h = (*h)->next;
-
-		free(h_hold);
-		len++;
-	}
-	*h = NULL;
-
-	return (len);
+newlist = malloc(size * sizeof(listint_t *));
+if (newlist == NULL)
+{
+free(list);
+exit(98);
+}
+for (i = 0; i < size - 1; i++)
+newlist[i] = list[i];
+newlist[i] = new;
+free(list);
+return (newlist);
 }
 
 /**
-  * find_listint_loop - checks whether a linked list contains a loop or not
-  * @head: head of the loop
-  * Return: 1 if there is a loop or 0 if not
-  */
-listint_t *find_listint_loop(listint_t *head)
+ * free_listint_safe - frees a listint_t linked list.
+ * @head: double pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t free_listint_safe(listint_t **head)
 {
-	listint_t *slow_p, *fast_p;
+size_t i, num = 0;
+listint_t **list = NULL;
+listint_t *next;
 
-	slow_p = fast_p = head;
-	while (slow_p && fast_p && fast_p->next)
-	{
-		slow_p = slow_p->next;
-		fast_p = fast_p->next->next;
-		if (slow_p == fast_p)
-		{
-			slow_p = head;
-			while (slow_p != fast_p)
-			{
-				slow_p = slow_p->next;
-				fast_p = fast_p->next;
-			}
-			return (fast_p);
-		}
-	}
-
-	return (NULL);
+if (head == NULL || *head == NULL)
+return (num);
+while (*head != NULL)
+{
+for (i = 0; i < num; i++)
+{
+if (*head == list[i])
+{
+*head = NULL;
+free(list);
+return (num);
+}
+}
+num++;
+list = _ra(list, num, *head);
+next = (*head)->next;
+free(*head);
+*head = next;
+}
+free(list);
+return (num);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -37,5 +37,6 @@ size_t print_listint_safe(const listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
 listint_t *find_loop(listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
+listint_t **_ra(listint_t **list, size_t size, listint_t *new);
 
 #endif
